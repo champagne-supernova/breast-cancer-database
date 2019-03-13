f540b401b2ffd587a844bfde2ee9a3d2032c95e8
champagne-supernova <38125265+champagne-supernova@users.noreply.github.com>
2019-03-13 14:34:42 +0800

    Add files via upload

---
+++ static/data/chrOGR/chrOGRs.js
@@ -0,0 +1,30 @@
+// the first name of each line in this json code means domestic cucumber chromosome id, and the second one is for wild
+// len = l/100000
+// l : length
+// wild.ogr = dom.ogr +.2
+//d_len d_gen: domestic gene length and gene num
+// w_len d_gen: wild gene length and gene num, D1 with the same d_l,w_l,d_gene, w_gene are ortholog chr.
+
+var data = [
+            {name: "D1", len:291, ogr: 1, d_l:29149675, d_gene:3554, w_l: 30058199, w_gene: 3498},
+            {name: "W1", len: 301, ogr: 1.2, d_l:29149675, d_gene:3554, w_l: 30058199, w_gene: 3498},
+    
+            {name: "D2", len:231, ogr: 2, d_l:23174626, d_gene:2993, w_l:24040433, w_gene:2797},
+            {name: "W2", len: 240, ogr: 2.2, d_l:23174626, d_gene:2993, w_l:24040433, w_gene:2797},
+    
+            {name: "D3", len:398, ogr: 3, d_l:39782674, d_gene:4976, w_l:40686695, w_gene:4775},
+            {name: "W3", len: 407, ogr: 3.2, d_l:39782674, d_gene:4976, w_l:40686695, w_gene:4775},
+            
+            {name: "D4", len:234, ogr: 4, d_l:23425844, d_gene:2891, w_l:24620846, w_gene:2784},
+            {name: "W4", len: 246, ogr: 4.2, d_l:23425844, d_gene:2891, w_l:24620846, w_gene:2784},
+            
+            {name: "D5", len:280, ogr: 5, d_l:28023477, d_gene:3233, w_l:29059648, w_gene:3157},
+            {name: "W5", len: 291, ogr: 5.2, d_l:28023477, d_gene:3233, w_l:29059648, w_gene:3157},
+            
+            {name: "D6", len:290, ogr: 6, d_l:29076228, d_gene:3994, w_l:30272445, w_gene:3938},
+            {name: "W6", len: 303, ogr: 6.2, d_l:29076228, d_gene:3994, w_l:30272445, w_gene:3938},
+            
+            {name: "D7", len:192, ogr: 7, d_l:19226500, d_gene:2498, w_l:19785952, w_gene:2350},
+            {name: "W7", len: 198, ogr: 7.2, d_l:19226500, d_gene:2498, w_l:19785952, w_gene:2350}
+            
+];  
